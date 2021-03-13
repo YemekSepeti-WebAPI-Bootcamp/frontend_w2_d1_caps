0fd7bf1a879c59438b84655adba6af4abf07227b
mrturkay <muratturkaym@gmail.com>
2021-03-13 14:16:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      memes: null,
+    };
+
+    console.log("constructor");
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedFromState", props, state);
+    return props;
+  }
+
+  componentDidMount() {
+    // fetch()
+    // this.setState({ deneme: "component did mount" });
+    console.log("comonentDidMount");
+  }
+
+  render() {
+    // console.log("render", this.state);
+    return <div className="App">{this.state.deneme}</div>;
+  }
 }
 
 export default App;
